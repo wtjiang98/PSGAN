c710187bf0cceff831de933587227388a02ea539
Zhaoyi Wan <wanzhaoyi@megvii.com>
2020-07-15 07:30:28 +0800

    Remove unneccesary dependence

--- concern/image.py
+++ concern/image.py
@@ -1,10 +1,9 @@
 import numpy as np
 import cv2
-from smart_path import smart_path
 from io import BytesIO
 
 
-def load_image(path: smart_path):
+def load_image(path):
     with path.open("rb") as reader:
         data = np.fromstring(reader.read(), dtype=np.uint8)
         img = cv2.imdecode(data, cv2.IMREAD_COLOR)
